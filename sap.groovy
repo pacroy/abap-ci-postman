ca468b9d6f04d5c3c857708f3b4847c3431ece4a
pacroy <pacroy@gmail.com>
2017-12-23 09:37:03 +0700

    Split groovy script + updated Postman script

---
+++ sap.groovy
@@ -0,0 +1,57 @@
+def abap_unit_coverage(HOST,CREDENTIAL,PACKAGE,COVERAGE) {	
+	println "CREDENTIAL=" + CREDENTIAL
+
+	withCredentials([usernamePassword(credentialsId: CREDENTIAL, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+		stage('ABAP Unit and Code Coverage') {
+			dir('sap-pipeline') {
+				bat "newman run abap_unit_coverage.postman_collection.json --insecure --bail " +
+				"--environment SAP_Pipeline.postman_environment.json " +
+				"--timeout-request 120000 " +
+				"--global-var host=$HOST " +
+				"--global-var username=$USERNAME " +
+				"--global-var password=$PASSWORD " +
+				"--global-var package=$PACKAGE " +
+				"--global-var coverage_min=$COVERAGE "
+			}
+		}
+	}
+}
+
+def abap_sci(HOST,CREDENTIAL,PACKAGE) {	
+	println "CREDENTIAL=" + CREDENTIAL
+	
+	withCredentials([usernamePassword(credentialsId: CREDENTIAL, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {	
+		stage('ABAP Code Inspector') {
+			dir('sap-pipeline') {
+					bat "newman run abap_sci.postman_collection.json --insecure --bail " +
+					"--environment SAP_Pipeline.postman_environment.json " +
+					"--timeout-request 120000 " +
+					"--global-var host=$HOST " +
+					"--global-var username=$USERNAME " +
+					"--global-var password=$PASSWORD " +
+					"--global-var package=$PACKAGE " 
+			}
+		}
+	}
+}
+
+def sap_api_test(HOST,CREDENTIAL) {	
+	println "CREDENTIAL=" + CREDENTIAL
+	
+	withCredentials([usernamePassword(credentialsId: CREDENTIAL, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+		stage('SAP API Tests') {
+			try {
+				bat "newman run SimpleRESTTest.postman_collection.json --insecure --bail " + 
+				"--environment NPL.postman_environment.json " + 
+				"--timeout-request 10000 " +
+				"--global-var username=$USERNAME " + 
+				"--global-var password=$PASSWORD "
+			} catch(e) {
+				skip_pipeline = true
+				currentBuild.result = 'FAILURE'
+			}
+		}
+	}
+}
+
+return this
